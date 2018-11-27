4d824fd9ec3628406ff9f18900cfa16cdfc5809c
guochao <chao.guo@aiyingli.com>
2018-11-27 11:40:20 +0800

    惯例入职第一工程

---
+++ TestProduct/TestProductTests/TestProductTests.m
@@ -0,0 +1,39 @@
+//
+//  TestProductTests.m
+//  TestProductTests
+//
+//  Created by AYLiOS on 2018/11/27.
+//  Copyright © 2018年 AYLiOS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestProductTests : XCTestCase
+
+@end
+
+@implementation TestProductTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
